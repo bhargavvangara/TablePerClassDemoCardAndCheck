70091cda5696fae0d31cc35e36d136eb2a927ae1
bhargavvangara <bhargavvangara5@gmail.com>
2025-05-06 22:36:25 +0530

    initial commit

---
+++ src/main/java/com/jpademo/tableperclass/card/and/check/TablePerClassDemoCardAndCheckApplication.java
@@ -0,0 +1,13 @@
+package com.jpademo.tableperclass.card.and.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablePerClassDemoCardAndCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablePerClassDemoCardAndCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpademo/tableperclass/card/and/check/TablePerClassDemoCardAndCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpademo.tableperclass.card.and.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablePerClassDemoCardAndCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
